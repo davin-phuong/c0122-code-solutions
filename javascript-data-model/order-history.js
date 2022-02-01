27c9bbcd153b94f0b1f624b57990461b34cbf502
Davin Phuong <davinphuong@yahoo.com>
2022-02-01 13:08:58 -0800

    in progress

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,48 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    orderNumber: '114-3941689-87772232',
+    total: '$34.00',
+    shipTo: 'JS Masher',
+    delivered: 'Delivered Aug 8, 2020',
+    returnWindow: 'Return windown closed on Sep 7,2020',
+    price: '$31.55',
+    item: ['JavaScript for impatient programmers',
+      {
+        author: 'Rauschmayer, Dr. Axel'
+      }]
+  },
+  {
+    orderPlaced: 'July 19, 200',
+    orderNumber: '113-9984268-1280257',
+    total: '$44.53',
+    shipTo: 'JS Masher',
+    delivered: 'Delivered Jul 20, 2020',
+    returnWindow: 'Return windown closed on Aug 19,2020',
+    price: '$41.33',
+    item: ['The Timeless Way of Building',
+      {
+        author: 'Alexander, Christopher'
+      }]
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    orderNumber: '114-2875557-95059409',
+    total: '$17.22',
+    shipTo: 'JS Masher',
+    delivered: 'Delivered Jul 7, 2020',
+    returnWindow: 'Return windown closed on Aug 5,2020',
+    price: '$15.98',
+    item: 'Gamecube'
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    orderNumber: '113-2883177-2648248',
+    total: '$138.93',
+    shipTo: 'JS Masher',
+    delivered: 'Delivered Jul 5, 2020',
+    returnWindow: 'Return windown closed on Aug 4,2020',
+    price: ['$94.95', '$33.99'],
+    item: ['GameCube Controller', 'The Art of Sql']
+  }
+];
