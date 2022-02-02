7c9119cbcc3aa85667fc0909e99f1123746b8e97
Davin Phuong <davinphuong@yahoo.com>
2022-02-01 16:44:24 -0800

    Completed javascript-data-model exercise.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -2,47 +2,70 @@ var orderHistory = [
   {
     orderPlaced: 'August 4, 2020',
     orderNumber: '114-3941689-87772232',
-    total: '$34.00',
+    total: 34.00,
     shipTo: 'JS Masher',
     delivered: 'Delivered Aug 8, 2020',
-    returnWindow: 'Return windown closed on Sep 7,2020',
-    price: '$31.55',
-    item: ['JavaScript for impatient programmers',
+    item: [
       {
-        author: 'Rauschmayer, Dr. Axel'
+        category: 'book',
+        title: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        returnWindow: 'Sep 7,2020',
+        price: 31.55
       }]
   },
   {
     orderPlaced: 'July 19, 200',
     orderNumber: '113-9984268-1280257',
-    total: '$44.53',
+    total: 44.53,
     shipTo: 'JS Masher',
     delivered: 'Delivered Jul 20, 2020',
-    returnWindow: 'Return windown closed on Aug 19,2020',
-    price: '$41.33',
-    item: ['The Timeless Way of Building',
+    item: [
       {
-        author: 'Alexander, Christopher'
+        category: 'book',
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnWindow: 'Aug 19,2020',
+        price: 41.33
       }]
   },
   {
     orderPlaced: 'July 4, 2020',
     orderNumber: '114-2875557-95059409',
-    total: '$17.22',
+    total: 17.22,
     shipTo: 'JS Masher',
     delivered: 'Delivered Jul 7, 2020',
-    returnWindow: 'Return windown closed on Aug 5,2020',
-    price: '$15.98',
-    item: 'Gamecube'
+    item: [
+      {
+        category: 'electronic',
+        title: 'GameCube Controller Adapter',
+        author: null,
+        returnWindow: 'Aug 5,2020',
+        price: 15.98
+      }
+    ]
   },
   {
     orderPlaced: 'July 3, 2020',
     orderNumber: '113-2883177-2648248',
-    total: '$138.93',
+    total: 138.93,
     shipTo: 'JS Masher',
     delivered: 'Delivered Jul 5, 2020',
-    returnWindow: 'Return windown closed on Aug 4,2020',
-    price: ['$94.95', '$33.99'],
-    item: ['GameCube Controller', 'The Art of Sql']
+    item: [
+      {
+        category: 'electronic',
+        title: 'GameCube Controller',
+        author: null,
+        returnWindow: 'Aug 4,2020',
+        price: 94.95
+      },
+      {
+        category: 'book',
+        title: 'The Art of Sql',
+        author: 'Faroult, Stephanie',
+        returnWindow: 'Aug 4,2020',
+        price: 33.99
+      }
+    ]
   }
 ];
