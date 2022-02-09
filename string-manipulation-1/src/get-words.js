02b1a7837ece6715075c62d1405ee457b30e4146
Davin Phuong <davinphuong@yahoo.com>
2022-02-08 21:42:35 -0800

    removed split and utilized a for loop

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -2,16 +2,30 @@
 
 function getWords(string) {
   var words = [];
+  var currentWord = '';
+
+  if (string === '') {
+    return [];
+  }
+
   for (var i = 0; i < string.length; i++) {
     if (string[i] !== ' ') {
-      return string.split(' ');
+      currentWord += string[i];
+    } else {
+      words.push(currentWord);
+      currentWord = '';
     }
   }
+  words.push(currentWord);
   return words;
 }
 
 /*
-1. create storage for empty array of empty words
-2. if string does not contain an empty space, break each word in the string and return as an array
-3. if string contains empty space, return array of empty words
+1. create storage for words
+2. create storage for current word
+3. look for words and if there aren't any, return empty storage of words
+4. if there is a word, look for space between each character
+5. if there is no space, add each letter to current word
+6. if there is a space, add current word to words list then clear out the word
+7. give back list of words
 */
