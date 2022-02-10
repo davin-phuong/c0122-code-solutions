884d0490b2b59dc0105ebb88f92e11918ef90e39
Davin Phuong <davinphuong@yahoo.com>
2022-02-09 20:50:15 -0800

    made changes to is-vowel to check fewer comparisons

--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,15 +1,20 @@
 /* exported isVowel */
 
 function isVowel(char) {
-  if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ||
-  char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U'
-  ) {
-    return true;
+  var lowerChar = char.toLowerCase();
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  for (var i = 0; i < vowels.length; i++) {
+    if (lowerChar === vowels[i]) {
+      return true;
+    }
   }
   return false;
 }
 
 /*
-1. if character contains a, e, i, o, u or A, E, I, O, U return true
-2. else return false
+1. convert character to lowercase letter
+2. create list of vowels
+3. if character matches list of vowels, return true
+4. if character does not match list of vowels, return false
 */
