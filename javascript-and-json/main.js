6ede09ba2db609e394c68c8f69395c90f8dfd9bd
Davin Phuong <davinphuong@yahoo.com>
2022-02-11 14:51:40 -0800

    Completed javascript-and-json.

---
+++ javascript-and-json/main.js
@@ -0,0 +1,34 @@
+var bookshelf = [
+  {
+    isbn: '978-1491929483',
+    title: 'Introduction to JavaScript Object Notation: A To-the-Point Guide to JSON',
+    author: 'Lindsay Bassett'
+  },
+  {
+    isbn: '978-1484218624',
+    title: 'JSON Quick Syntax Reference',
+    author: 'Wallace Jackson'
+  },
+  {
+    isbn: '978-0692232699',
+    title: 'Build APIs You Won\'t Hate: Everyone and their dog wants an API, so you should probably learn how to build them',
+    author: 'Phil Sturgeon & Laura Bohill'
+  }
+];
+
+console.log('typeof bookshelf:', typeof bookshelf);
+
+var bookshelfJsonString = JSON.stringify(bookshelf);
+console.log('JSON string of bookshelf:', bookshelfJsonString);
+
+var studentJsonString = {
+  'number id': '004',
+  'string name': 'Mocha'
+};
+
+console.log('studentJsonString:', studentJsonString);
+console.log('typeof studentJsonString:', typeof studentJsonString);
+
+var parsedObject = JSON.parse(JSON.stringify(studentJsonString));
+console.log('parsedObject:', parsedObject);
+console.log('typeof parsedObject:', typeof parsedObject);
