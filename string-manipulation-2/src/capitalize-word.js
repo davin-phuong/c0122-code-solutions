365b6f788ea93c701299a83b96976dc89d79f306
Davin Phuong <davinphuong@yahoo.com>
2022-02-13 21:27:22 -0800

    in progress

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,1 +1,11 @@
 /* exported capitalizeWord */
+
+/*
+- separate the first character from the word
+- capitalize the first character of the word
+- make all other characters of the word lowercase
+- combine word back together
+- if the word is any variation of JavaScript,
+capitalize the word at index 0 and index 4
+- return the capitalized word
+*/
--- string-manipulation-2/src/truncate.js
+++ string-manipulation-2/src/truncate.js
@@ -3,5 +3,12 @@
 function truncate(length, string) {
   var truncatedWord = '';
 
+  truncatedWord = string.slice(string[length], length) + '...';
   return truncatedWord;
 }
+
+/*
+- create storage for truncated word
+- remove letters beginning at the length of the word then add an ellipsis
+- return truncated word
+*/
