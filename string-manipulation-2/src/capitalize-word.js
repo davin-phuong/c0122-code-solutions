2b4d8ac6654032128602ade600f99d6c516332f0
Davin Phuong <davinphuong@yahoo.com>
2022-02-19 23:10:42 -0800

    in progress: completed capitalize-word and ransom-case

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,11 +1,20 @@
 /* exported capitalizeWord */
 
+function capitalizeWord(word) {
+  var capitalWord = word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
+
+  if (capitalWord === 'Javascript') {
+    return 'JavaScript';
+  } else {
+    return capitalWord;
+  }
+}
+
 /*
 - separate the first character from the word
 - capitalize the first character of the word
 - make all other characters of the word lowercase
 - combine word back together
-- if the word is any variation of JavaScript,
-capitalize the word at index 0 and index 4
+- if the word is any variation of Javascript, return JavaScript
 - return the capitalized word
 */
