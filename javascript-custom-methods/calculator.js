b94759f756d85c9c0a70b60eeeead10165059fa2
Davin Phuong <davinphuong@yahoo.com>
2022-02-21 11:44:01 -0800

    in progress

---
+++ javascript-custom-methods/calculator.js
@@ -0,0 +1,30 @@
+/* exported calculator */
+
+var calculator = {
+  add: function (x, y) {
+    return (x + y);
+  },
+
+  subtract: function (x, y) {
+    return (x - y);
+  },
+
+  multiply: function (x, y) {
+    return (x * y);
+  },
+
+  divide: function (x, y) {
+    return (x / y);
+  },
+
+  square: function (x) {
+    return (x * x);
+  },
+
+  sumAll: function (numbers) {
+    var sum = 0;
+    var i = 0;
+    return (sum += numbers[i]);
+  }
+
+};
